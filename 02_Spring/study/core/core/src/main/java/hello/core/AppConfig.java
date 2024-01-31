db2a56edc52be61e60b32b18b3cd2162929bf3f2
Nabong <99lnh@naver.com>
2024-01-31 21:03:13 +0900

    20240131_핵심원리 4 스프링 빈

--- 02_Spring/study/core/core/src/main/java/hello/core/AppConfig.java
+++ 02_Spring/study/core/core/src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     // cmd + option + M => 역할의 따른 구분이 필요 추상화 메서드
-    private static MemoryMemberRepository memberRepository() {
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();

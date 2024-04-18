474ac2a2101c78bada07ccd5bbd7a3da2778b499
Nabong <99lnh@naver.com>
2024-04-18 23:45:48 +0900

    20240418_프로젝트3_폰트,이미지,스타일 설정

--- 01_React/section12/src/App.jsx
+++ 01_React/section12/src/App.jsx
@@ -5,9 +5,8 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from './pages/Notfound';
 
-// 1. "/" : 모든 페이지를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+import { getEmtionImage } from './util/get-emotion-image';
+
 function App() {
 
   const nav = useNavigate();
@@ -17,10 +16,13 @@ function App() {
   }
   return (
   <>
-    {/* 
-      페이지가 새로 고침 되지는 않고 필요한 요소만 렌더링 된다. 
-      Link는 a 태그 같은 페이지 이동, 하지만 a태그 이용하면 csr 됨
-    */}
+    <div>
+      <img src={getEmtionImage(1)} />
+      <img src={getEmtionImage(2)} />
+      <img src={getEmtionImage(3)} />
+      <img src={getEmtionImage(4)} />
+      <img src={getEmtionImage(5)} />
+    </div>
     <div>
       <Link to={"/"}>Home</Link>
       <Link to={"/new"}>New</Link>
